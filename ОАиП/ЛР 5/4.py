25ba22756f595b400198710c9aa281780f234c9f
KatyaRyachkina <yryachkina@inbox.ru>
2025-10-02 00:15:50 +0500

    4.py

--- ОАиП/ЛР 5/4.py
+++ ОАиП/ЛР 5/4.py
@@ -1,1 +1,7 @@
-
+numbers = []
+for i in range(5):num = float(input(f"Введите число {i+1}: "))
+numbers.append(num)
+min_num = min(numbers)
+max_num = max(numbers)
+print(f"Минимальное число: {min_num}")
+print(f"Максимальное число: {max_num}")
