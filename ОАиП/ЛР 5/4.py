a6265294feec41ff1c72954e39ab0f641d9710d9
KatyaRyachkina <yryachkina@inbox.ru>
2025-10-10 12:55:29 +0500

    4.py

--- ОАиП/ЛР 5/4.py
+++ ОАиП/ЛР 5/4.py
@@ -1,7 +1,5 @@
-numbers = []
-for i in range(5):num = float(input(f"Введите число {i+1}: "))
-numbers.append(num)
-min_num = min(numbers)
-max_num = max(numbers)
-print(f"Минимальное число: {min_num}")
-print(f"Максимальное число: {max_num}")
+num = input("Введите 5 чисел через пробел: ").split()
+minnum = min(map(int, num))
+maxnum = max(map(int, num))
+print("Минимальное число:", minnum)
+print("Максимальное число:", maxnum)
