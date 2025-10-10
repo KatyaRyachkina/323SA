fbd79fb9da2dae999369a8586ab371c9adb9e32c
KatyaRyachkina <yryachkina@inbox.ru>
2025-10-10 12:51:43 +0500

    3.py

--- ОАиП/ЛР 5/3.py
+++ ОАиП/ЛР 5/3.py
@@ -1,10 +1,4 @@
-fio_input = input("Введите ФИО через пробел: ")
-fio_parts = fio_input.split()
-
-if len(fio_parts) == 3:
-    surname = fio_parts[0]
-    name = fio_parts[1]
-    patronymic = fio_parts[2]
-    print(surname.upper())
-    print(name.upper())
-    print(patronymic.upper())
+fio = input("Введите ФИО: ").split()
+print(fio[0])
+print(fio[1])
+print(fio[2])
