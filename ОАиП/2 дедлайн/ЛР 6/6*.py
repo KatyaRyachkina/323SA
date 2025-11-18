21412e2b8e43b0bb166b66157be9f7dfff96659a
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-18 21:22:15 +0500

    Create 6*.py

---
+++ ОАиП/2 дедлайн/ЛР 6/6*.py
@@ -0,0 +1,7 @@
+def is_palindrome(text):
+"""Функция проверяет, является ли строка палиндромом"""
+    clean_text = text.lower().replace(" ", "")
+ print(is_palindrome("топот"))
+    print(is_palindrome("python"))
+    print(is_palindrome("Топот"))  
+    print(is_palindrome("А роза упала на лапу Азора"))
