b67ae0511c62be6ad3576bc60b926f73f815f00c
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-18 20:12:59 +0500

    Delete ОАиП/2 дедлайн/5.py

--- ОАиП/2 дедлайн/5.py
+++
@@ -1,9 +0,0 @@
-text = input("Введите строку: ")
-glas = "аеёиоуыэюяaeiouyАЕЁИОУЫЭЮЯAEIOUY"
-result = ""
-n = 0
-while n < len(text):
-if text[n] not in glas:
-result += text[n]
-n += 1 
-print("Строка без гласных:", result)
