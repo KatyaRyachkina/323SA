fd0318666277d4f688589922e89215f9be9708aa
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-18 19:56:07 +0500

    Delete ОАиП/Отчет ЛР 7 directory

--- ОАиП/Отчет ЛР 7/4.py
+++
@@ -1,9 +0,0 @@
-year = int(input("Введите год: "))
-if year % 400 == 0:
-print(year, "високосный")
-elif year % 100 == 0:
-print(year, "не високосный")
-elif year % 4 == 0:
-print(year, "високосный")
-else:
-print(year, "не високосный")
