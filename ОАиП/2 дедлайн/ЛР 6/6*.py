f56abb4d869cd2b22eec0cf34133b5b1f5ceddd8
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 16:02:34 +0500

    Update 6*.py

--- ОАиП/2 дедлайн/ЛР 6/6*.py
+++ ОАиП/2 дедлайн/ЛР 6/6*.py
@@ -1,7 +1,8 @@
 def is_palindrome(text):
-"""Функция проверяет, является ли строка палиндромом"""
     clean_text = text.lower().replace(" ", "")
- print(is_palindrome("топот"))
-    print(is_palindrome("python"))
-    print(is_palindrome("Топот"))  
-    print(is_palindrome("А роза упала на лапу Азора"))
+    return clean_text == clean_text[::-1]
+
+print(is_palindrome("топот"))
+print(is_palindrome("python"))
+print(is_palindrome("Топот"))  
+print(is_palindrome("А роза упала на лапу Азора"))
