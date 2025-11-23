69e73e248d1cc13cf4d49b0bff9c56b2a165887f
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 14:45:42 +0500

    Update 2.py

--- ОАиП/1 дедлайн/ЛР 7/2.py
+++ ОАиП/1 дедлайн/ЛР 7/2.py
@@ -1,12 +1,12 @@
 password = input("Придумайте свой пароль: ")
 confirm = input("Подтвердите пароль: ")
 if password == confirm:
-print("Пароль сохранен")
+    print ("Пароль сохранен")
 else:
-print("Пароли не совпадают")
+     print ("Пароли не совпадают")
 exit()
 login = input("Введите пароль: ")
 if login == password:
-print("Access")
+     print("Access")
 else:
-print("Denied")
+     print("Denied")
