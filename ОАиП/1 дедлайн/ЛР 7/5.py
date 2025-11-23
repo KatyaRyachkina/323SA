41be3f38a57bd66e8244a3727104be980a1d4bbd
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 14:50:51 +0500

    Update 5.py

--- ОАиП/1 дедлайн/ЛР 7/5.py
+++ ОАиП/1 дедлайн/ЛР 7/5.py
@@ -1,8 +1,10 @@
 import math
+
 data = input("Введите число1, знак и число2: ").split()
 num1 = float(data[0])
 sign = data[1]
 num2 = float(data[2])
+
 if sign == '+':
     result = num1 + num2
 elif sign == '-':
@@ -13,7 +15,7 @@ elif sign == '/':
     if num2 != 0:
         result = num1 / num2
     else:
-        print("Деление на 0 невозможно")
+        result = "Деление на 0 невозможно"
 elif sign == '%':
     result = num1 % num2
 elif sign == '//':
@@ -32,4 +34,5 @@ elif sign == '"':
     result = math.sqrt(num1)
 else:
     result = "Неизвестная операция"
+
 print("Результат:", result)
