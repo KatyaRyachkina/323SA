759795bda62f74e007e0a2d981668b885cb515f0
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 15:21:40 +0500

    Update 4.py

--- ОАиП/2 дедлайн/ЛР 3/4.py
+++ ОАиП/2 дедлайн/ЛР 3/4.py
@@ -3,11 +3,13 @@ products = {
     "Мышь": {"price": 1000, "sold": 45},
     "Клавиатура": {"price": 2500, "sold": 30},
     "Монитор": {"price": 30000, "sold": 8}
-max = 0
+}
+
+max_sold = 0
 best = ""
 for product, info in products.items():
-    if info["sold"] > max:
-        max = info["sold"]
+    if info["sold"] > max_sold:
+        max_sold = info["sold"]
         best = product
 print("Самый продаваемый товар:", best)
 
