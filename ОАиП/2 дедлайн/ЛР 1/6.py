bd75b013598244f76333079f0faae37de45237cc
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 15:03:33 +0500

    Update 6.py

--- ОАиП/2 дедлайн/ЛР 1/6.py
+++ ОАиП/2 дедлайн/ЛР 1/6.py
@@ -1,11 +1,12 @@
- symbol = input("Введите символ: ")
+symbol = input("Введите символ: ")
 height = int(input("Введите высоту: "))
 width = int(input("Введите ширину: "))
 i = 0
+
 while i < height:
-j = 0
-while j < width:
-print(symbol, end="")
-j += 1
-print()
-i += 1
+    j = 0
+    while j < width:
+        print(symbol, end="")
+        j += 1
+    print()
+    i += 1
