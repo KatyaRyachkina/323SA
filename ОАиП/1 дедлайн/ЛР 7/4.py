ea23151fc99f42d1599b0b04030b38e950a533db
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 14:49:15 +0500

    Update 4.py

--- ОАиП/1 дедлайн/ЛР 7/4.py
+++ ОАиП/1 дедлайн/ЛР 7/4.py
@@ -1,9 +1,10 @@
 year = int(input("Введите год: "))
+
 if year % 400 == 0:
-print(year, "високосный")
+    print(year, "високосный")
 elif year % 100 == 0:
-print(year, "не високосный")
+    print(year, "не високосный")
 elif year % 4 == 0:
-print(year, "високосный")
+    print(year, "високосный")
 else:
-print(year, "не високосный")
+    print(year, "не високосный")
