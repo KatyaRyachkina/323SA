67176c5cd272cb354c7794625c7a7544f6534e67
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 18:04:30 +0500

    Update 1.py

--- ОАиП/3 дедлайн/ЛР 1/1.py
+++ ОАиП/3 дедлайн/ЛР 1/1.py
@@ -1,1 +1,15 @@
+def create_counter():
+    count = 0
+    
+    def counter():
+        nonlocal count
+        count += 1
+        return count
+      
+    return counter
 
+if __name__ == "__main__":
+    counter = create_counter()
+    print(counter())
+    print(counter())
+    print(counter())
