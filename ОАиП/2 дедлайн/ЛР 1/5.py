565b731b2f3397f797ab24589db090960bac7578
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 15:00:17 +0500

    Update 5.py

--- ОАиП/2 дедлайн/ЛР 1/5.py
+++ ОАиП/2 дедлайн/ЛР 1/5.py
@@ -2,8 +2,10 @@ text = input("Введите строку: ")
 glas = "аеёиоуыэюяaeiouyАЕЁИОУЫЭЮЯAEIOUY"
 result = ""
 n = 0
+
 while n < len(text):
-if text[n] not in glas:
-result += text[n]
-n += 1 
+    if text[n] not in glas:
+        result += text[n]
+    n += 1 
+
 print("Строка без гласных:", result)
