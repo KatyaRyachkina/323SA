3af43af64604daca79518d439527b3041138a86d
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 14:55:13 +0500

    Update 3.py

--- ОАиП/2 дедлайн/ЛР 1/3.py
+++ ОАиП/2 дедлайн/ЛР 1/3.py
@@ -2,13 +2,15 @@ stroka = input("Введите строку: ").lower().replace(" ","")
 left = 0
 right = len(stroka) - 1
 palindrom = True
+
 while left < right:
-if stroka[left] != stroka[right]:
-palindrom = False
-break
-left += 1
-right -= 1 
+    if stroka[left] != stroka[right]:
+        palindrom = False
+        break
+    left += 1
+    right -= 1 
+
 if palindrom:
-print("Это палиндром")
+    print("Это палиндром")
 else:
-print("Это не палиндром")
+    print("Это не палиндром")
