4767eed5438c177ec31cbe9a7e1c5b0c7df1b693
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 16:08:36 +0500

    Update 3_3.py

--- ОАиП/2 дедлайн/ЛР 7/3_3.py
+++ ОАиП/2 дедлайн/ЛР 7/3_3.py
@@ -1,17 +1,8 @@
 def filter_scores(data_dict: dict[str, float | int], min_value: float | int) -> dict[str, float | int]:
-    """Фильтрует словарь с оценками по минимальному значению.
-    
-    Args:
-        data_dict: Словарь с оценками.
-        min_value: Минимальное значение оценки.
-    
-    Returns:
-        dict: Отфильтрованный словарь."""
     result = {}
-    
     for key, value in data_dict.items():
         if value >= min_value:
-            result[key] = value     
+            result[key] = value
     return result
 
 scores = {"math": 95, "history": 78, "english": 88, "art": 92}
