63bee5855c0bde154919013a384cb467d909af63
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 16:00:07 +0500

    Update 4.py

--- ОАиП/2 дедлайн/ЛР 6/4.py
+++ ОАиП/2 дедлайн/ЛР 6/4.py
@@ -1,5 +1,4 @@
 def find_max(num1, num2):
-    """Функция возвращает большее из двух чисел"""
     return num1 if num1 > num2 else num2
-max_num = find_max(101, 25)
-print(max_num)   
+
+print(find_max(101, 25))
