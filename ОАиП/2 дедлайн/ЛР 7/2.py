2626894d7e7132cff7dfabcfe6450af2522fac7c
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 16:05:07 +0500

    Update 2.py

--- ОАиП/2 дедлайн/ЛР 7/2.py
+++ ОАиП/2 дедлайн/ЛР 7/2.py
@@ -1,12 +1,7 @@
 def build_user_profile(user_id: int, **kwargs: str) -> dict[str, str | int]:
-    """Создает профиль пользователя на основе ID и дополнительной информации.
-    Args:
-        user_id: Уникальный идентификатор пользователя.
-        **kwargs: Произвольные именованные аргументы с дополнительной информацией.
-    Returns:
-    dict: Словарь с профилем пользователя."""
     profile = {"user_id": user_id}
     profile.update(kwargs)
     return profile
+
 profile = build_user_profile(101, name="Анна", status="online", email="anna@example.com")
 print(profile)
