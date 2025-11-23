7a1b33dcd820596781426056045d1d421ad9af56
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 15:14:00 +0500

    Update 5.py

--- ОАиП/2 дедлайн/ЛР 2/5.py
+++ ОАиП/2 дедлайн/ЛР 2/5.py
@@ -1,17 +1,21 @@
 n = int(input("Сколько чисел вы хотите ввести? "))
 numbers = []
+
 for i in range(1, n + 1):
-num = int(input(f"Введите число {i}: "))
-numbers.append(num)
-max = max(numbers)
-min = min(numbers)
+    num = int(input(f"Введите число {i}: "))
+    numbers.append(num)
+
+max_num = max(numbers)
+min_num = min(numbers)
 average = sum(numbers) / len(numbers)
 count = 0
+
 for num in numbers:
-  if num > average:
-    count += 1
+    if num > average:
+        count += 1
+
 print("\nРезультаты:")
-print(f"Максимальное: {max}")
-print(f"Минимальное: {min}")
+print(f"Максимальное: {max_num}")
+print(f"Минимальное: {min_num}")
 print(f"Среднее: {average}")
 print(f"Чисел больше среднего: {count}")
