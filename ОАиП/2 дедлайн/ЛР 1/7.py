b25ae2f1964ae93c4c89fa3a9dd7c43f13345486
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 15:03:16 +0500

    Update 7.py

--- ОАиП/2 дедлайн/ЛР 1/7.py
+++ ОАиП/2 дедлайн/ЛР 1/7.py
@@ -1,10 +1,11 @@
 symbol = input("Символ: ")
 height = int(input("Высота: "))
 width = int(input("Ширина: "))
+
 for i in range(height):
-    for j in range(width):
-   if i == 0 or i == height - 1 or j == 0 or j == width - 1:
-   print(symbol, end="")
-     else:
-    print(" ", end="")
-    print()
+    for j in range(width):
+        if i == 0 or i == height - 1 or j == 0 or j == width - 1:
+            print(symbol, end="")
+        else:
+            print(" ", end="")
+    print()
