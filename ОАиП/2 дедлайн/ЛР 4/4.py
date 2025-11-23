5f5528c2936d1213785d71cffb96e3367c3e5d32
KatyaRyachkina <yryachkina@inbox.ru>
2025-11-23 15:31:41 +0500

    Update 4.py

--- ОАиП/2 дедлайн/ЛР 4/4.py
+++ ОАиП/2 дедлайн/ЛР 4/4.py
@@ -3,24 +3,27 @@ vysokaia = temperatury[0]
 for temp in temperatury:
     if temp > vysokaia:
         vysokaia = temp
+
 nizkaia = temperatury[0]
 for temp in temperatury:
     if temp < nizkaia:
         nizkaia = temp
-summa = 0
-for temp in temperatury:
-    summa += temp
+
+summa = sum(temperatury)
 sred = summa / len(temperatury)
+
 dney_vishe_srednei = 0
 for temp in temperatury:
     if temp > sred:
         dney_vishe_srednei += 1
-sort = temperatury.copy()  
-sort.sort()              
+
+sort = sorted(temperatury)
+
 farengeity = []
 for temp in temperatury:
     f = temp * 9/5 + 32
     farengeity.append(round(f, 1))
+
 print("Температуры:", temperatury)
 print("Самая высокая:", vysokaia, "°C")
 print("Самая низкая:", nizkaia, "°C")
